4db2526b209c692af30f1a89576809228d134ebf
DiegoVivas17 <70493750+DiegoVivas17@users.noreply.github.com>
2021-08-19 17:16:04 -0500

    Finalizada clase estudiante 2.0

--- EDListas6/src/edlistas6/Estudiante.java
+++ EDListas6/src/edlistas6/Estudiante.java
@@ -13,12 +13,12 @@ public class Estudiante {
     
     private String nombre;
     private int edad;
-
+    
     public String getNombre() {
         return nombre;
     }
 
-    public void setNombre(String nombre) {
+    private void setNombre(String nombre) {
         this.nombre = nombre;
     }
 
@@ -26,7 +26,12 @@ public class Estudiante {
         return edad;
     }
 
-    public void setEdad(int edad) {
+    private void setEdad(int edad) {
         this.edad = edad;
+    }
+    
+    public Estudiante(String nombre, int edad){
+        setEdad(edad);
+        setNombre(nombre);
     }
 }
