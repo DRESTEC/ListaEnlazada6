5b01237a9e5a2d07f2ae0c31142191e641737e6e
DiegoVivas17 <70493750+DiegoVivas17@users.noreply.github.com>
2021-08-19 23:55:32 -0500

    Finalizacion clase Main

--- EDListas6/src/edlistas6/Main.java
+++ EDListas6/src/edlistas6/Main.java
@@ -4,6 +4,7 @@
  * and open the template in the editor.
  */
 package edlistas6;
+import java.util.Scanner;
 
 /**
  *
@@ -15,8 +16,55 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
-        System.out.println("Hola");
+        
+        ListaEstudiante lista = new ListaEstudiante();
+        
+        int num=400;
+        
+        while (num!=0){
+            num = lista.Menu();
+            
+            if(num==1){
+                Scanner leer= new Scanner(System.in);
+                System.out.println("Nombre:");
+                String nombre = leer.next();
+                
+                Scanner leeredad= new Scanner(System.in);
+                System.out.println("Edad:");
+                int edad = leeredad.nextInt();
+                
+                Nodo n = new Nodo(nombre,edad);
+                
+                lista.agregar(n);
+                
+            }else if(num==2){
+                Scanner leer= new Scanner(System.in);
+                System.out.println("Indice:");
+                int indice = leer.nextInt();
+                
+                Nodo n = lista.buscarPorIndice(indice);
+                System.out.println(n.mostrarInformacion());  
+            }else if(num==3){
+                Scanner leer= new Scanner(System.in);
+                System.out.println("Indice:");
+                int indice = leer.nextInt();
+                
+                lista.eliminarPorIndice(indice);
+            }else if(num==4){
+                System.out.println(lista.obtenerPrimero().mostrarInformacion());
+            }else if(num==5){
+                System.out.println(lista.obtenerUltimo().mostrarInformacion());
+            }else if(num==6){
+                lista.Vaciar();
+            }else if(num==7){
+                lista.eliminarUltimo();
+            }else if(num==8){
+                lista.imprimir();
+            }else if(num==9){
+                System.out.println("Promedio: "+lista.calcularPromedioEdad());
+                lista.calcularEdadMenor();
+                lista.calcularEdadMayor();
+            }
+        }
     }
-    
 }
